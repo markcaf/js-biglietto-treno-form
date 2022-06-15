f0cb3bfe45a71474b69494ec13ba62df0d76ce11
markcaf <marcocafassog@gmail.com>
2022-06-15 18:16:22 +0200

    Milestone 2, recap with output on page

--- js/script.js
+++ js/script.js
@@ -8,9 +8,11 @@ calcButton.addEventListener("click", function()
         // Catturo i dati inseriti
         console.log("Km da percorrere: " + document.getElementById("user-distance").value);
         const userDistance = parseInt(document.getElementById("user-distance").value);
+        document.querySelector(".user-distance-output").innerHTML += userDistance +"km";
 
         console.log("Età utente: " + document.getElementById("user-age").value);
         const userAge = parseInt(document.getElementById("user-age").value);
+        document.querySelector(".user-age-output").innerHTML += userAge + " anni";
 
         //Calcolo del prezzo del biglietto in base ai km
         let ticketPrice = 0.26 * userDistance;
@@ -19,11 +21,14 @@ calcButton.addEventListener("click", function()
         if (userAge < 18){
             ticketPrice = ticketPrice - (ticketPrice * 15) / 100;
             console.log("Sconto 15% per minorenni");
+            document.querySelector(".type-of-sale").innerHTML = "15% per minorenni";
         }   else if (userAge >= 65){
                 ticketPrice = ticketPrice - (ticketPrice * 35) / 100;
                 console.log("Sconto 35% per over65");
+                document.querySelector(".type-of-sale").innerHTML = "35% per over65";
             }
         ticketPrice = ticketPrice.toFixed(2);
         console.log("Prezzo finale: €" + ticketPrice);
+        document.querySelector(".ticket-price").innerHTML += "€" + ticketPrice;
     }
 )
