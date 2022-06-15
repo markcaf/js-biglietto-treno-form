ec8da8779043a7fc8ce6275a214f505dab16cf10
markcaf <marcocafassog@gmail.com>
2022-06-15 17:22:35 +0200

    calc of ticket's price based on the user distance

--- js/script.js
+++ js/script.js
@@ -8,12 +8,14 @@ calcButton.addEventListener("click", function()
         // Catturo i dati inseriti
         console.log("Km da percorrere: " + document.getElementById("user-distance").value);
         const userDistance = parseInt(document.getElementById("user-distance").value);
-        console.log("variabile userDistance creata: " + userDistance + "km");
 
         console.log("Età utente: " + document.getElementById("user-age").value);
         const userAge = parseInt(document.getElementById("user-age").value);
-        console.log("variabile userAge creata: " + userAge + " anni");
 
         //Calcolo del prezzo del biglietto in base ai km
+        let ticketPrice = 0.26 * userDistance;
+        ticketPrice = ticketPrice.toFixed(2);
+        console.log("Prezzo in base ai km: €" + ticketPrice);
+
     }
 )
